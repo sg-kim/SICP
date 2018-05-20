6bad005f76160d0af4be58c0cc1d63bc23dd49d2
sg-kim <sgkim08@gmail.com>
2018-05-20 12:17:04 +0900

    functional programming - average calculator

--- constraint_network_averager.scm
+++ constraint_network_averager.scm
@@ -11,71 +11,6 @@ Image saved on Monday May 19, 2014 at 9:55:33 PM
 ;`C-h' means: hold down the Ctrl key and type `h'.
 ;Package: (user)
 
-(define (multiplier m1 m2 product)
-  (define (process-new-value)
-    (cond
-     ((or (and (has-value? m1) (= (get-value m1) 0))
-	  (and (has-value? m2) (= (get-value m2) 0))) (set-value! product 0 me))
-      ((and (has-value? m1) (has-value? m2)) (set-value! product (* (get-value m1) (get-value m2)) me))
-      ((and (has-value? m1) (has-value? product)) (set-value! m2 (/ (get-value product) (get-value m1)) me))
-      ((and (has-value? m2) (has-value? product)) (set-value! m1 (/ (get-value product) (get-value m2)) me))
-      )
-    )
-  (define (process-forget-value)
-    (forget-value! product me)
-    (forget-value! m1 me)
-    (forget-value! m2 me)
-    (process-new-value)
-    )
-  (define (me op)
-    (cond
-     ((equal? op 'I-have-a-value) (process-new-value))
-     ((equal? op 'I-lost-my-value) (process-forget-value))
-     (else (error "undefined op - MULTIPLIER - " op))
-     )
-    )
-  (connect m1 me)
-  (connect m2 me)
-  (connect product me)
-  me
-)
-
-(define (adder a b sum)
-  (define (process-new-value)
-    (cond (
-	   (and (has-value? a) (has-value? b)) (set-value! sum (+ (get-value a) (get-value b)) me)
-	   (and (has-value? a) (has-value? sum)) (set-value! b (- (get-value sum) (get-value a)) me)
-	   (and (has-value? b) (has-value? sum)) (set-value! a (- (get-value sum) (get-value b)) me)
-	   )
-	  )
-    )
-  (define (process-forget-value)
-    (forget-value! sum me)
-    (forget-value! a me)
-    (forget-value! b me)
-    (process-new-value))
-  (define (me op)
-    (cond 
-     ((equal? op 'I-have-a-value) (process-new-value))
-     ((equal? op 'I-lost-my-value) (process-forget-value))
-     (else (error "Unknown op -- ADDER - " op))
-     )
-    )
-  (connect a me)
-  (connect b me)
-  (connect sum me)
-  me
-)
-
-(define (constant value connector)
-  (define (me op)
-    (error "Undefined operation -- CONSTANT - " op)
-    )
-  (connect connector me)
-  (set-value! connector value me)
-  me
-)
-
 (define (make-connector)
   (let (
 	 (value false)
@@ -120,6 +55,77 @@ Image saved on Monday May 19, 2014 at 9:55:33 PM
 	me
 	)
 )
+;Value: make-connector
+
+(define (adder a b sum)
+  (define (process-new-value)
+    (cond (
+	   (and (has-value? a) (has-value? b)) (set-value! sum (+ (get-value a) (get-value b)) me)
+	   (and (has-value? a) (has-value? sum)) (set-value! b (- (get-value sum) (get-value a)) me)
+	   (and (has-value? b) (has-value? sum)) (set-value! a (- (get-value sum) (get-value b)) me)
+	   )
+	  )
+    )
+  (define (process-forget-value)
+    (forget-value! sum me)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me op)
+    (cond 
+     ((equal? op 'I-have-a-value) (process-new-value))
+     ((equal? op 'I-lost-my-value) (process-forget-value))
+     (else (error "Unknown op -- ADDER - " op))
+     )
+    )
+  (connect a me)
+  (connect b me)
+  (connect sum me)
+  me
+)
+
+(define (multiplier m1 m2 product)
+  (define (process-new-value)
+    (cond
+     ((or (and (has-value? m1) (= (get-value m1) 0))
+	  (and (has-value? m2) (= (get-value m2) 0))) (set-value! product 0 me))
+      ((and (has-value? m1) (has-value? m2)) (set-value! product (* (get-value m1) (get-value m2)) me))
+      ((and (has-value? m1) (has-value? product)) (set-value! m2 (/ (get-value product) (get-value m1)) me))
+      ((and (has-value? m2) (has-value? product)) (set-value! m1 (/ (get-value product) (get-value m2)) me))
+      )
+    )
+  (define (process-forget-value)
+    (forget-value! product me)
+    (forget-value! m1 me)
+    (forget-value! m2 me)
+    (process-new-value)
+    )
+  (define (me op)
+    (cond
+     ((equal? op 'I-have-a-value) (process-new-value))
+     ((equal? op 'I-lost-my-value) (process-forget-value))
+     (else (error "undefined op - MULTIPLIER - " op))
+     )
+    )
+  (connect m1 me)
+  (connect m2 me)
+  (connect product me)
+  me
+)
+
+(define (constant value connector)
+  (define (me op)
+    (error "Undefined operation -- CONSTANT - " op)
+    )
+  (connect connector me)
+  (set-value! connector value me)
+  me
+)
+;Value: adder
+
+;Value: multiplier
+
+;Value: constant
 
 (define (for-each-except exception procedure list)
   (define (loop items)
@@ -130,6 +136,7 @@ Image saved on Monday May 19, 2014 at 9:55:33 PM
     )
   (loop list)
 )
+;Value: for-each-except
 
 (define (probe name connector)
   (define (print-probe value)
@@ -155,36 +162,20 @@ Image saved on Monday May 19, 2014 at 9:55:33 PM
   (connect connector me)
   me
 )
+;Value: probe
 
-(define (inform-about-value constraint)
-  (constraint 'I-have-a-value)
-)
+(define (inform-about-value constraint) (constraint 'I-have-a-value))
+(define (inform-about-no-value constraint) (constraint 'I-lost-my-value))
+;Value: inform-about-value
 
-(define (inform-about-no-value constraint)
-  (constraint 'I-lost-my-value)
-)
+;Value: inform-about-no-value
+
 
 (define (has-value? connector) (connector 'has-value?))
 (define (get-value connector) (connector 'value))
 (define (set-value! connector new-value informant) ((connector 'set-value!) new-value informant))
 (define (forget-value! connector retractor) ((connector 'forget) retractor))
 (define (connect connector new-constraint) ((connector 'connect) new-constraint))
-;Value: multiplier
-
-;Value: adder
-
-;Value: constant
-
-;Value: make-connector
-
-;Value: for-each-except
-
-;Value: probe
-
-;Value: inform-about-value
-
-;Value: inform-about-no-value
-
 ;Value: has-value?
 
 ;Value: get-value
@@ -195,99 +186,79 @@ Image saved on Monday May 19, 2014 at 9:55:33 PM
 
 ;Value: connect
 
-(define a (make-connector))
-(define b (make-connector))
-(define c (make-connector))
-;Value: a
-
-;Value: b
-
-;Value: c
-
-(define (averager a b c)
-  (let
-      (
-       (u (make-connector))
-       (v (make-connector))
-       )
+(define (averager)
+  (let (
+	(a (make-connector))
+	(b (make-connector))
+	(c (make-connector))
+	(u (make-connector))
+	(v (make-connector))
+	)
     (adder a b u)
     (multiplier c v u)
     (constant 2 v)
-    'ok
+    (probe 'Input_A a)
+    (probe 'Input_B b)
+    (probe 'Output_C c)
+    (define (module_averager op . args)
+      (cond
+       ((equal? op 'set-a!) (set-value! a (car args) (cadr args)))
+       ((equal? op 'set-b!) (set-value! b (car args) (cadr args)))
+       ((equal? op 'set-c!) (set-value! c (car args) (cadr args)))
+       ((equal? op 'forget-a!) (forget-value! a (car args)))
+       ((equal? op 'forget-b!) (forget-value! b (car args)))
+       ((equal? op 'forget-c!) (forget-value! c (car args)))
+       ((equal? op 'get-a) (get-value a))
+       ((equal? op 'get-b) (get-value b))
+       ((equal? op 'get-c) (get-value c))
+       ((equal? op 'has-a?) (has-value? a))
+       ((equal? op 'has-b?) (has-value? b))
+       ((equal? op 'has-c?) (has-value? c))
+       ((equal? op 'show-net)
+	(display '(a b <--> adder <--> u))
+	(newline)
+	(display '(c v <--> multiplier <--> u))
+	(newline)
+	(display '(v = 2)))
+       )
+      )
+    module_averager
     )
 )
 ;Value: averager
 
-(probe 'Input_A a)
-(probe 'Input_B b)
-(probe 'Output_C c)
-;Value 11: #[compound-procedure 11 me]
+(define avg (averager))
+;Value: avg
 
-;Value 12: #[compound-procedure 12 me]
-
-;Value 13: #[compound-procedure 13 me]
-
-(averager a b c)
-;Value: ok
-
-(set-value! a 10 'user)
+(avg 'set-a! 10 'user)
+(avg 'set-b! 20 'user)
 
 Probe: input_a = 10
 ;Value: done
 
-(set-value! b 6 'user)
 
-Probe: output_c = 8
-Probe: input_b = 6
+Probe: input_b = 20
+Probe: output_c = 15
 ;Value: done
 
-(forget-value! a 'user)
+(avg 'forget-a! 'user)
+(avg 'forget-b! 'user)
 
-Probe: output_c = ?
 Probe: input_a = ?
+Probe: output_c = ?
 ;Value: done
 
-(forget-value! b 'user)
 
 Probe: input_b = ?
 ;Value: done
 
-(set-value! a 20 'user)
-(set-value! c 15 'user)
+(avg 'set-a! 17.0 'user)
+(avg 'set-b! 10.0 'user)
 
-Probe: input_a = 20
+Probe: input_a = 17.
 ;Value: done
 
 
-Probe: output_c = 15
+Probe: input_b = 10.
+Probe: output_c = 13.5
 ;Value: done
-
-(forget-value! a 'user)
-(forget-value! c 'user)
-
-Probe: input_a = ?
-;Value: done
-
-
-Probe: output_c = ?
-;Value: done
-
-(set-value! a 25 'user)
-(set-value! b 6 user)
-
-Probe: input_a = 25
-;Value: done
-
-;Unbound variable: user
-;To continue, call RESTART with an option number:
-; (RESTART 3) => Specify a value to use instead of user.
-; (RESTART 2) => Define user to a given value.
-; (RESTART 1) => Return to read-eval-print level 1.
-;Start debugger? (y or n): n
-
-(set-value! b 6 'user)
-
-Probe: output_c = 31/2
-Probe: input_b = 6
-;Value: done
-
